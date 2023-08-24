0408074c0c2147f4ed65622283a321d669f762f6
miraclemakerz <vajgenthraprabakaran@gmail.com>
2023-08-24 21:51:45 +0800

    pokedex24

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const pokedex = document.getElementById('pokedex');
+const pokedex = document.getElementById("pokedex");
+
+console.log(pokedex);
 
 const fetchPokemon = () => {
     const promises = [];
@@ -6,12 +8,13 @@ const fetchPokemon = () => {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
+
     Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites["front_default"],
+            type: data.types.map((type) => type.type.name).join(",")
         }));
         displayPokemon(pokemon);
     });
@@ -21,7 +24,8 @@ const displayPokemon = (pokemon) => {
     console.log(pokemon);
     const pokemonHTMLString = pokemon
         .map(
-            (pokeman) => `
+            (pokeman) =>
+                `
         <li class="card">
             <img class="card-image" src="${pokeman.image}"/>
             <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
@@ -29,7 +33,7 @@ const displayPokemon = (pokemon) => {
         </li>
     `
         )
-        .join('');
+        .join("");
     pokedex.innerHTML = pokemonHTMLString;
 };
 
